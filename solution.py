db730179c4187783ebfbc00ecbee32b489d90e55
HSangwaiya <ashsangwaiya@gmail.com>
2024-09-02 00:02:01 +0530

    if a phoneme is changed, it wont be changed again

--- solution.py
+++ solution.py
@@ -15,26 +15,60 @@ class Agent:
 
     def generate_neighbor1(self, state, environment, start_time, current_cost):
         words = state.split()
+        n = len(words)
+        for i in range(n):
+            word = words[i]
+            changed = []
+            while True:
+                # neighbors = []
+                state_updated = False
+                for phoneme, substitutes in self.subsitutions.items():
+                    phoneme_pos = [j for j in range(
+                        len(word)) if word.startswith(phoneme, j) and j not in changed]
+                    if phoneme_pos:
+                        for substitute in substitutes:
+                            for pos in phoneme_pos:
+                                new_word = word[:pos] + \
+                                    substitute + word[pos+len(phoneme):]
+                                new_state = ' '.join(
+                                    words[:i] + [new_word] + words[i+1:])
 
-        for i, word in enumerate(words):
-            for phoneme, substitutes in self.subsitutions.items():
-                phoneme_pos = [j for j in range(
-                    len(word)) if word.startswith(phoneme, j)]
-                if phoneme_pos:
-                    for substitute in substitutes:
-                        for pos in phoneme_pos:
-                            new_word = word[:pos] + \
-                                substitute + word[pos+len(phoneme):]
-                            new_state = ' '.join(
-                                words[:i] + [new_word] + words[i+1:])
-
-                            new_cost = environment.compute_cost(new_state)
-                            if new_cost < current_cost:
-                                state = new_state
-                                current_cost = new_cost
-                                print("Current state:", state)
-                            elif time.time() - start_time > 240:
-                                return state, current_cost
+                                new_cost = environment.compute_cost(new_state)
+                                if new_cost < current_cost:
+                                    state = new_state
+                                    current_cost = new_cost
+                                    state_updated = True
+                                    words[i] = new_word
+                                    word = new_word
+                                    print("Current state:", state)
+                                    if (len(phoneme) == len(substitute)):
+                                        changed.append(pos)
+                                    elif (len(phoneme) > len(substitute)):
+                                        m = len(changed)
+                                        for j in range(m):
+                                            if changed[j] > pos:
+                                                changed[j] += len(substitute) - \
+                                                    len(phoneme)
+                                        changed.append(pos)
+                                    else:
+                                        m = len(changed)
+                                        for j in range(m):
+                                            if changed[j] > pos:
+                                                changed[j] += len(substitute) - \
+                                                    len(phoneme)
+                                        changed.append(pos)
+                                        changed.append(pos+1)
+                                    break
+                                elif time.time() - start_time > 240:
+                                    return state, current_cost
+                            if state_updated:
+                                break
+                        if state_updated:
+                            break
+                    if state_updated:
+                        break
+                if not state_updated:
+                    break
 
         return state, current_cost
 
@@ -42,7 +76,6 @@ class Agent:
         if (time.time() - start_time > 240):
             return state, current_cost
 
-        # Beginning of the sentence
         for word in self.vocabulary:
             new_state = word + ' ' + state
             new_cost = environment.compute_cost(new_state)
@@ -54,7 +87,6 @@ class Agent:
             elif time.time() - start_time > 240:
                 return state, current_cost
 
-        # End of the sentence
         for word in self.vocabulary:
             new_state = state + ' ' + word
             new_cost = environment.compute_cost(new_state)
