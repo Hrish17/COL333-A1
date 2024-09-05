78a3a3e21b50e6afbde8885d978a763200011f57
HSangwaiya <ashsangwaiya@gmail.com>
2024-09-05 10:59:07 +0530

    pre_final

--- solution.py
+++ solution.py
@@ -1,7 +1,3 @@
-import time
-import json
-
-
 class Agent:
     def __init__(self, phoneme_table, vocabulary):
         self.phoneme_table = phoneme_table
@@ -14,10 +10,7 @@ class Agent:
                     self.subsitutions[value] = []
                 self.subsitutions[value].append(key)
 
-    # def local_beam_search(self, beam, start_time, current_cos):
-    #     pass
-
-    def generate_neighbor1(self, state, environment, start_time, current_cost):
+    def generate_neighbor1(self, state, environment, current_cost):
         words = state.split()
         n = len(words)
         for i in range(n):
@@ -46,8 +39,6 @@ class Agent:
                                         neighbor_cost = new_cost
                                         neighbor_pos = pos
                                         neighbor_word = new_word
-                                elif time.time() - start_time > 240:
-                                    return state, current_cost
                 if neighbor_state == "" or len(changed) == len(word):
                     break
                 if neighbor_state != "":
@@ -76,33 +67,28 @@ class Agent:
 
         return state, current_cost
 
-    def generate_neighbor2(self, state, environment, start_time, current_cost):
-        if (time.time() - start_time > 240):
-            return state, current_cost
+    def generate_neighbor3(self, state, environment, current_cost):
+        temp_state = state
 
         for word in self.vocabulary:
             new_state = word + ' ' + state
             new_cost = environment.compute_cost(new_state)
             if new_cost < current_cost:
-                state = new_state
+                temp_state = new_state
                 current_cost = new_cost
-                print("Current state:", state)
-                break
-            elif time.time() - start_time > 240:
-                return state, current_cost
+                print("Current state:", temp_state)
+
+        state = temp_state
 
         for word in self.vocabulary:
             new_state = state + ' ' + word
             new_cost = environment.compute_cost(new_state)
             if new_cost < current_cost:
-                state = new_state
+                temp_state = new_state
                 current_cost = new_cost
-                print("Current state:", state)
-                break
-            elif time.time() - start_time > 240:
-                return state, current_cost
+                print("Current state:", temp_state)
 
-        return state, current_cost
+        return temp_state, current_cost
 
     def asr_corrector(self, environment):
         initial_state = environment.init_state
@@ -111,13 +97,23 @@ class Agent:
         current_cost = initial_cost
         self.best_state = current_state
 
-        start_time = time.time()
         print("Initial state:", environment.init_state)
 
         current_state, current_cost = self.generate_neighbor1(
-            current_state, environment, start_time, current_cost)
+            current_state, environment, current_cost)
         print("Neighbor 1:", current_state)
 
-        current_state, current_cost = self.generate_neighbor2(
-            current_state, environment, start_time, current_cost)
-        print("Neighbor 2:", current_state)
+        while (self.best_state != current_state):
+            self.best_state = current_state
+
+            current_state, current_cost = self.generate_neighbor1(
+                current_state, environment, current_cost)
+            print("Neighbor 2:", current_state)
+
+        self.best_state = current_state
+
+        current_state, current_cost = self.generate_neighbor3(
+            current_state, environment, current_cost)
+        print("Neighbor 3:", current_state)
+
+        self.best_state = current_state
